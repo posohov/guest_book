89a4b24d1a64faceb18b1e7b7b4a4123327a1982
Yurii Posohov <mangura3078@gmail.com>
2020-04-20 11:43:57 +0300

    Check you last commit

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
 <?php
 	//открываем соединение с базой данных и получаем обьект соединения с базой данных
 	$conn = mysqli_connect("localhost", "root", "", "guest_book");
+
 	//устанавливаем кодировку символов "utf8"
 	mysqli_set_charset($conn, "utf8");
 
@@ -36,61 +37,69 @@
 	}
 	//проверяем метод запроса POST и делаем запрос к базе данных	
 	/*if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	   	$name = $_POST["name"];
+	   	$name = $_POST["name"];//
 		$lastname = $_POST["lastname"];
 		$message = $_POST["message"];
-		$query = "INSERT INTO users (name, lastname, message) VALUES (?,?,?)";
-		$stmt = $conn->prepare($query); //подготавливает запрос  к базе данных
-		$stmt->bind_param("sss", $name, $lastname, $message); //выполнения запроса
+		$query = "INSERT INTO users (name, lastname, message) VALUES (?,?,?)";//
+		$stmt = $conn->prepare($query); //подготавливает запрос  к базе данных и возвращает связанный с этим запросом объект
+		$stmt->bind_param("sss", $name, $lastname, $message); //Привязка переменных к параметрам подготавливаемого запроса
+		$stmt->execute();
 			
 		//проверка запроса на выполнение
 		if (!$stmt->execute()) {
 	    echo "Выполнить запрос не удалось: (" . $stmt->errno . ") " . $stmt->error;
 		}
 	}*/
+	//создаём массив ошибок заполнения формы
 	$errors = [];
+
+	//проверяем на наличие данных  в форме
 	if (!empty($_POST)) {
 		if(empty($_POST['name'])) {
-			$errors[] = "Введите имя";
+			$errors['name'] = "Введите имя";
 		}
 		if (empty($_POST['lastname'])) {
-			$errors[] = "Введите фамилию";
+			$errors['lastname'] = "Введите фамилию";
 		}
 		if (empty($_POST['message'])) {
-			$errors[] = "Hello";
+			$errors['message'] = "Hello";
 		}
-		if (empty($errors)) {
+		if (!isset($errors)) {
 		$name = $_POST['name'];
 		$lastname = $_POST['lastname'];
 		$message = $_POST['message'];
 		$query = "INSERT INTO users (name, lastname, message) VALUES (?,?,?)";
 		$stmt = $conn->prepare($query); //подготавливает запрос  к базе данных
-		$stmt->bind_param("sss", $name, $lastname, $message); //выполнения запроса
-
-		if(!$stmt->execute()) {
-	    echo "Выполнить запрос не удалось: (" . $stmt->errno . ") " . $stmt->error;
+		$stmt->bind_param("sss", $name, $lastname, $message); //привязка переменных к параметрам подготавливаемого запроса
+		$stmt->execute();//выполняет подготовленный запрос
+		} 
+		else{
+			//перебираем массив ошибок
+			foreach($errors as $key => $value) {
+			$errors[$key] = $key;
+			echo "<h3> Enter: -" . $errors[$key] . '</h3><br>';//выводим ошибки заполнения формы
+			}
 		}
-	} else {
-		die("Заполните данные формы");
 	}
-		$sql = "SELECT  name, lastname, message, created_at FROM users ORDER BY created_at DESC Limit 10";
-		$result = $conn->query($sql);
 		
-	}
+		$sql = "SELECT  name, lastname, message, created_at FROM guest_book.users ORDER BY created_at DESC LIMIT 10";
+		$result = $conn->query($sql);//выполнения запроса к базе данных
+
 	//проверяем на наличие данных в БД
-	if ($result->num_rows() > 0) {
-	   // выводим данные каждой строки
+	if ($result->num_rows > 0) {
+	   // выводим данные каждой строки в виде таблицы
 		echo '<table class="table table-striped">';
 		echo '<tr><th>Name</th><th>Lastname</th><th>Message</th><th>Created</th><tr>';
+		//извлекаем ассоциативный массив в виде таблицы
 	   	while($row = $result->fetch_assoc()) {
-	   		echo '<tr><td>' . $row['name'] . '</td><td></td><td>' . $row["message"] . '</td><td>' . $row["created_at"] . '</td></tr>';
+	   		echo '<tr><td>' . $row['name'] . '</td><td>' . $row['lastname'] .'</td><td>' . $row["message"] . '</td><td>' . $row["created_at"] . '</td></tr>';
 	    }
 	     echo '</table>';
 	} else {
 	    echo "0 results";
 	}
 
-		//закрываем соединение с БД
+	//закрываем соединение с БД
 	$conn->close();
 ?>
 	
